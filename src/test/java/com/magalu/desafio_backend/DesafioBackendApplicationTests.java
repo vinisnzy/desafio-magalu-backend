c25a9d17ccd51664e741221caed1a20553c52ad1
Vinícius de Paula <manoseilacara123@gmail.com>
2025-07-24 11:13:56 -0300

    initial commit

---
+++ src/test/java/com/magalu/desafio_backend/DesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magalu.desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
